894d21336a1b84eb70dd120a145bed4a399bc40a
Shengyou Fan <shengyoufan@gmail.com>
2020-11-17 13:46:11 +0800

    design posts table

--- database/migrations/2020_11_17_054049_create_posts_table.php
+++ database/migrations/2020_11_17_054049_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('is_feature');
+
             $table->timestamps();
         });
     }
