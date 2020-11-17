f7320f9a2abf05abce9e28cef617d8b38fda17e0
Shengyou Fan <shengyoufan@gmail.com>
2020-11-17 13:48:17 +0800

    setup model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'title',
+        'content',
+        'is_feature',
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'content' => 'string',
+        'is_feature' => 'boolean',
+    ];
 }
